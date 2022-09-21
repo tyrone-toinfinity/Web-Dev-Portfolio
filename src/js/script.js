99025fd5f8db86d26a9f9654f19429179636f331
Tyrone Woody <bobbywoody7@gmail.com>
2022-09-21 13:11:44 -0600

    projects updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+// Test
+console.log("Hello World!");
+
+// Scroll Animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+console.log(hiddenElements);
